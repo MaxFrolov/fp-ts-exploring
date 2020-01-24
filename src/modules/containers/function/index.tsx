2c8b9ba99388015fbfc64e3b0d1ed70e4c84050d
venumteam <maxtor2808@gmail.com>
2020-01-24 19:13:31 +0200

    function final content

--- src/modules/containers/function/index.tsx
+++ src/modules/containers/function/index.tsx
@@ -37,7 +37,7 @@ const FunctionContainer = () => {
   }
   
   not<number>(Boolean)(3) // ${notTestNum(3)} -> <boolean>
-  not<number>(Boolean)(3) // ${notTestNum(0)} -> <boolean>
+  not<number>(Boolean)(0) // ${notTestNum(0)} -> <boolean>
   not<string>(Boolean)('') // ${notTestStr('')} -> <boolean>
   not<string>(Boolean)('text') // ${notTestStr('text')} -> <boolean>
   `
@@ -111,6 +111,64 @@ const FunctionContainer = () => {
   flip<number, number, number>((a, b) => a - b)(1, 3) // ${flipTest(1, 3)} -> <number>
   `
 
+  // flow
+  const flowTest = FN.flow<[number, number], number, number>((a, b): number => a + b, (c) => c * 2)
+  const flowTx = `
+  function flow<A extends Array<unknown>, B>(ab: (...a: A) => B): (...a: A) => B
+  function flow<A extends Array<unknown>, B, C>(ab: (...a: A) => B, bc: (b: B) => C): (...a: A) => C
+  ... // 9 arguments max
+  
+  flow<[number, number], number, number>((a, b): number => a + b, (c) => c * 2)(2, 3) // ${flowTest(2, 3)} -> <number>
+  `
+
+  // tuple
+  const tupleTx = `
+  function tuple<T extends Array<any>>(...t: T): T {
+    return t
+  }
+  
+  tuple<number[]>(1, 2) // [${FN.tuple<number[]>(1, 2)}] -> <array>
+  `
+
+  // increment
+  const incrementTx = `
+  function increment(n: number): number {
+    return n + 1
+  }
+  
+  increment(2) // ${FN.increment(2)} -> <number>
+  `
+
+  // decrement
+  const decrementTx = `
+  function decrement(n: number): number {
+    return n - 1
+  }
+  
+  decrement(2) // ${FN.decrement(2)} -> <number>
+  `
+
+  // tupled
+  const tupleTest = FN.tupled((x: number, y: number): number => x + y)
+  const tupledTx = `
+  function tupled<A extends Array<unknown>, B>(f: (...a: A) => B): (a: A) => B {
+    return a => f(...a)
+  }
+  
+  tupled((x: number, y: number): number => x + y)([1, 2]) // ${tupleTest([1, 2])} -> <number>
+  `
+
+  // untupled
+  const untupledTest = FN.untupled((x: number[]) => x.join(', '))
+
+  const untupledTx = `
+  function untupled<A extends Array<unknown>, B>(f: (a: A) => B): (...a: A) => B {
+    return (...a) => f(a)
+  }
+  
+  untupled((x: number[]) => x.join(', '))(1, 2]) // ${untupledTest(1, 2)} -> <number>
+  `
+
   return (
     <>
       <Title>FP-TS (function)</Title>
@@ -124,6 +182,12 @@ const FunctionContainer = () => {
       <CodeBlock label='constUndefined' codeTx={constUndefinedTx} />
       <CodeBlock label='constVoid' codeTx={constVoidTx} />
       <CodeBlock label='flip' codeTx={flipTx} />
+      <CodeBlock label='flow' codeTx={flowTx} />
+      <CodeBlock label='tuple' codeTx={tupleTx} />
+      <CodeBlock label='increment' codeTx={incrementTx} />
+      <CodeBlock label='decrement' codeTx={decrementTx} />
+      <CodeBlock label='tupled' codeTx={tupledTx} />
+      <CodeBlock label='untupled' codeTx={untupledTx} />
     </>
   )
 }
